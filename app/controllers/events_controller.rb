34ad2dda0867e339ec9c4b19c4cc73c1c40a916a
bamboo_octopus <bamboooctopus@gmail.com>
2022-02-01 15:12:14 -0500

    fleshed out events controller and events views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,39 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def show 
+        @event = Event.find(params[:id])
+    end
+
+    def new
+        @user = current_user
+        @event = @user.events.build
+        
+    end
+
+    def create
+        @user = current_user
+        @event = @user.events.build(event_params)
+
+        if @event.save
+            redirect_to root_path
+        else
+            render :new
+        end
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    private
+    def event_params
+        params.require(:event).permit(:name)
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
 
+    validates :name, presence: true
+
     belongs_to :creator, class_name: "User", foreign_key: "creator_id"
     has_many :attended_events, foreign_key: "event_id"
 end
