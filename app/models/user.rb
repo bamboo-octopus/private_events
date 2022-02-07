de0f22b08fe73f61f5a38f8ea3fa693b4880aacd
bamboo_octopus <bamboooctopus@gmail.com>
2022-02-06 19:46:52 -0500

    most of the views sorted out; invite submit button works

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
 
   has_many :events, foreign_key: "creator_id"
   has_many :attended_events, foreign_key: "attendee_id"
+
+  def username
+    return self.email.split('@')[0]
+  end
+  
 end
